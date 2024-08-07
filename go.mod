ec0573f652bbd38a4bc70b73b77cb7eef3e64a3e
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2024-08-07 19:44:42 +0700

    Add Codebase

---
+++ go.mod
@@ -0,0 +1,3 @@
+module studidevsecops/golang-sample
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Golang Sample, Selamat datang! \nApp Version: v1 \nBelajar Jenkins Deploy Golang")
+}
+
+func envPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List of Environment Variables:")
+	for _, env := range os.Environ() {
+		fmt.Fprintf(w, "- "+env+"\n")
+	}
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/env", envPage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func main() {
+	handleRequests()
+}
